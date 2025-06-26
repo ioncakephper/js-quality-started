ed9ca65477fec131b4b2e88a732778ca6ee90092
Ion Gireada <shytiger@yahoo.com>
2025-06-26 17:55:26 +0300

    refactor: clean up whitespace in add function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@
  * @returns {number} The sum of the two numbers.
  */
 function add(a, b) {
-
-
-                  
-                        return a + b;            
+  return a + b;
 }
 
 module.exports = add;
